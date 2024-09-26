4ec8ebab521f216b21a2ad7b0930ca5f0118d1f8
Geshan Manandhar <geshan@simplywallst.com>
2024-09-26 21:28:13 +1000

    Adds jest.user real timers to the test after each

--- test/dateHelper.test.js
+++ test/dateHelper.test.js
@@ -4,10 +4,12 @@ describe('dateHelper - Jest 26+', () => {
   //Jest 26 was released in May 2020, ref: https://github.com/jestjs/jest/releases/tag/v26.0.0
   describe('getDayOfYear', () => {
     beforeEach(() => {
-      jest.useFakeTimers().setSystemTime(new Date('2024-01-07'));
+      jest.useFakeTimers();
+      jest.setSystemTime(new Date('2024-01-07'));
     });
     afterEach(() => {
       jest.setSystemTime(jest.getRealSystemTime());
+      jest.useRealTimers();
     });
 
     it('should return 7 for 7-Jan as set with fake timers and system date', () => {      
